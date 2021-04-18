e60522e3f789c601b52b23a87f76c01be181087a
Hamza Hayd <dado.org@hotmail.com>
2021-04-18 16:03:25 +0200

    Managing cart and modal state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from "react";
 import Cart from "./components/Cart/Cart";
-import Header from "./components/Layout/Header"
+import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 const App = () => {
-    return (
-      <Fragment>
-        <Cart />
-        <Header />
-        <main>
-          <Meals />
-        </main>
-      </Fragment>
-    );
-}
+  const [cartIsShown, setCartIsShown] = useState(false);;
 
-export default App 
+  const showCartHandler = () => {
+    setCartIsShown(true);;
+  };;
+  const hideCartHandler = () => {
+    setCartIsShown(false);;
+  };
+  return (
+    <Fragment>
+      {cartIsShown &&  <Cart onClose={hideCartHandler} />  }
+      
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+};
+
+export default App;
