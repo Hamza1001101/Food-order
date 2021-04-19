f56b46cb5e5e29c051f4e8cba216f5683247b262
Hamza Hayd <dado.org@hotmail.com>
2021-04-19 18:54:28 +0200

    Handling errors

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -33,14 +33,19 @@ import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(true)
-  
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
   useEffect(() => {
     const fetchMeals = async () => {
-      
       const response = await fetch(
         "https://food-delivery-e285c-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -55,16 +60,27 @@ const AvailableMeals = () => {
           price: responseData[key].price,
         });
       }
-      setMeals(loadedMeals)
-      setIsLoading(false)
+      setMeals(loadedMeals);
+      setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return <section className={classes.MealsLoading}>Loading...</section>;
   }
 
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p> {httpError} </p>
+      </section>
+    );
+  }
   const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
