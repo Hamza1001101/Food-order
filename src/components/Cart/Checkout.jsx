db1aaddba3f3bfd90b4cd92a809355dcd98b2b9d
Hamza Hayd <dado.org@hotmail.com>
2021-04-22 11:33:51 +0200

    Finished project

--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -12,7 +12,7 @@ const Checkout = (props) => {
     city: true,
     postalCode: true,
   });
-
+ 
   const nameInputRef = useRef(); 
   const streetInputRef = useRef();
   const postalCodeInputRef = useRef();
